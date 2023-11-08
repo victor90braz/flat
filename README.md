3199f81f76e8f2e290892acdb323be1eed12d114
victor braz <victor.90braz@gmail.com>
2023-11-08 12:26:37 +0100

    use Tailwind CSS in Laravel and Vite

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 composer create-project laravel/laravel flat
 php artisan storage:link
+
+-   How to use Tailwind CSS in Laravel and Vite
+    https://youtu.be/qqIlkVuUC2Y?si=5y42DZc-SKTTwXHf
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,12 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="{{ asset('storage/images/favicon.png') }}" type="image/x-icon">
-    <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
+    @vite('resources/css/app.css')
     <title>Flat</title>
 </head>
 <body>
-    <h1>Flat</h1>
+    <h1 class="text-3xl font-bold underline">Flat</h1>
     <img src="{{ asset('storage/' . 'images/favicon.png') }}" alt="favicon" width="50">
 </body>
 </html>
