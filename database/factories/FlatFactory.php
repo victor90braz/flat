2cee47f08678a90a8af264f0c2ed68d462351972
victor braz <victor.90braz@gmail.com>
2023-11-09 15:46:57 +0100

    make flat model factory and controller

---
+++ database/factories/FlatFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Flat>
+ */
+class FlatFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
