5897a5564f6503cae3d0a6357f1985915f165100
victor braz <victor.90braz@gmail.com>
2023-11-09 17:25:12 +0100

    add dashboard page

--- resources/views/pages/layout.blade.php
+++ resources/views/pages/layout.blade.php
@@ -1,5 +1,22 @@
 @extends('app')
 
 @section('content')
-   <h1>Dashboard</h1>
+    <div class="container">
+        <h1>Dashboard</h1>
+
+        <ul class="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4">
+
+            @php
+                $flats = (new App\Models\Flat())->all();
+            @endphp
+
+            @foreach ($flats as $flat)
+                <li>
+                    @include('components.flats.template', [
+                        'flat' => $flat
+                    ])
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
