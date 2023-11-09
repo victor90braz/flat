8ac9754e6f92b904595ec14c4fe1a691c0105b02
victor braz <victor.90braz@gmail.com>
2023-11-09 11:19:31 +0100

    create a navigate component

--- resources/views/flats.blade.php
+++ resources/views/flats.blade.php
@@ -1,22 +1,5 @@
 @include('partial.header')
-    <nav class="bg-green-800">
-        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div class="relative flex h-16 items-center justify-between">
-            <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div class="flex flex-shrink-0 items-center">
-                    <img class="h-8 w-auto" src="{{ asset('storage/' . 'images/favicon.png') }}" alt="Logo Company">
-                    <h1 class="uppercase">flats</h1>
-                </div>
-              <div class="hidden sm:ml-6 sm:block">
-                <div class="flex space-x-4">
-                  <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
-                  <a href="/flats" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Flats</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-    </nav>
+@include('components.navigate')
 
     <div class="bg-white">
         <h2 class="text-2xl font-bold tracking-tight text-gray-900">All Flats</h2>
