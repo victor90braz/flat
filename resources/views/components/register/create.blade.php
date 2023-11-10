3c990b06b310b4dc35de9ae7f40ef240e2c47a25
victor braz <victor.90braz@gmail.com>
2023-11-10 17:18:07 +0100

    remove margin top

--- resources/views/components/register/create.blade.php
+++ resources/views/components/register/create.blade.php
@@ -4,12 +4,12 @@
     <div class="flex min-h-full flex-col justify-center px-6 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
             <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
-            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            <h2 class="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                 Register
             </h2>
         </div>
 
-        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
             <form class="space-y-6" action="/register" method="POST">
                 @csrf
                 <div>
