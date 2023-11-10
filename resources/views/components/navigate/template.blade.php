6eacf004e921dc98829ef43a37881719621ae59e
victor braz <victor.90braz@gmail.com>
2023-11-10 12:52:51 +0100

    add new sections to home page

--- resources/views/components/navigate/template.blade.php
+++ resources/views/components/navigate/template.blade.php
@@ -8,7 +8,7 @@
 
         <div class="hidden sm:block ml-6">
             <div class="flex space-x-4">
-                <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
+                <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
                 <a href="/flats" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Flats</a>
             </div>
         </div>
@@ -24,7 +24,7 @@
 
     <div class="hidden sm:hidden" id="mobile-menu">
         <div class="px-2 pt-2 pb-3 space-y-1">
-            <a href="/" class="block bg-gray-900 text-white rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
+            <a href="/" class="block bg-gray-900 text-white rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
             <a href="/flats" class="block bg-gray-900 text-white rounded-md px-3 py-2 text-base font-medium" aria-current="page">Flats</a>
         </div>
     </div>
--- resources/views/pages/layout.blade.php
+++ resources/views/pages/layout.blade.php
@@ -1,25 +1,30 @@
 @extends('app')
 
 @section('content')
-    <div class="bg-blue-500 text-white p-4 shadow-md">
-        <h1 class="text-center text-4xl font-bold mb-6">Welcome to Flats</h1>
+    <header class="bg-blue-500 text-white p-4 shadow-md">
+        <h1 class="text-center text-4xl font-bold mb-6">Welcome to <span class="text-red-600">Flats</span> Platform</h1>
 
-        <p>Welcome to a world where you'll discover the most beautiful flats. Our platform is designed to offer you an exquisite selection, tailored to your preferences.</p>
+        <section>
+            <p class="text-lg font-semibold">Explore Flats' Offerings</p>
+            <p>Join now to become a member and unlock exclusive benefits!</p>
+            <p>Whether you're seeking comfort, style, or modern living, we have the perfect flats waiting for you.  </p>
+        </section>
 
-        <p class="text-lg font-semibold">Explore Flats' Offerings</p>
-        <p>Join now to become a member and unlock exclusive benefits! Whether you're seeking comfort, style, or modern living, we have the perfect flats waiting for you.</p>
+        <nav class="mt-4">
+            <a href="#" class="inline-block bg-white text-blue-500 px-4 py-2 rounded-md mr-4">Register</a>
+            <a href="#" class="inline-block bg-white text-blue-500 px-4 py-2 rounded-md">Login</a>
+        </nav>
+    </header>
 
-        <a href="#" class="mt-2 inline-block bg-white text-blue-500 px-4 py-2 rounded-md">Register</a>
-        <a href="#" class="mt-2 inline-block bg-white text-blue-500 px-4 py-2 rounded-md">Login</a>
-    </div>
-
-    <ul class="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4 mt-6">
-        @foreach ($flats as $flat)
-            <li>
-                @include('components.flat.template', [
-                    'flat' => $flat
-                ])
-            </li>
-        @endforeach
-    </ul>
+    <main>
+        <ul class="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4 mt-6">
+            @foreach ($flats as $flat)
+                <li>
+                    @include('components.flat.template', [
+                        'flat' => $flat
+                    ])
+                </li>
+            @endforeach
+        </ul>
+    </main>
 @endsection
