2d7ba927f38aefa7ec05eadb673e2980bd1ea920
victor braz <victor.90braz@gmail.com>
2023-11-10 17:15:06 +0100

    create a login form

--- resources/views/components/register/create.blade.php
+++ resources/views/components/register/create.blade.php
@@ -50,7 +50,7 @@
 
             <p class="mt-10 text-center text-sm text-gray-500">
                 Already a member?
-                <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Log In!</a>
+                <a href="login" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Log In!</a>
             </p>
         </div>
     </div>
