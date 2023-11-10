afb9cac246a44231cf02710362c441e083d6fb7c
victor braz <victor.90braz@gmail.com>
2023-11-10 16:14:54 +0100

    create a register form

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
     public function index() {
-        dd('register created');
+
+        return view("components.register.index");
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,5 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [require("@tailwindcss/aspect-ratio")],
+    plugins: [require("@tailwindcss/aspect-ratio", "@tailwindcss/forms")],
 };
