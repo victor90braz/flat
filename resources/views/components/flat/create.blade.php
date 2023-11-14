8439782648f60a549928c1858f9bc2a5cea68bab
victor braz <victor.90braz@gmail.com>
2023-11-14 17:49:27 +0100

    update input layout

--- resources/views/components/flat/create.blade.php
+++ resources/views/components/flat/create.blade.php
@@ -19,11 +19,19 @@
         </div>
 
         <div>
-          <label for="price" class="block text-sm font-medium leading-6 text-gray-900">PRICE</label>
-          <div class="mt-2">
-            <input id="price" name="price" type="number" step="0.01" autocomplete="current-price" required class="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <label for="price" class="block text-sm font-medium leading-6 text-gray-900">Price</label>
+            <div class="relative mt-2 rounded-md shadow-sm">
+              <input type="text" name="price" id="price" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00">
+              <div class="absolute inset-y-0 right-0 flex items-center">
+                <label for="currency" class="sr-only">Currency</label>
+                <select id="currency" name="currency" class="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
+                  <option>USD</option>
+                  <option>CAD</option>
+                  <option>EUR</option>
+                </select>
+              </div>
+            </div>
           </div>
-        </div>
 
         <div>
           <label for="description" class="block text-sm font-medium leading-6 text-gray-900">DESCRIPTION</label>
