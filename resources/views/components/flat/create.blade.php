307972f8cab58f526c58cde2f71e3228c305696a
victor braz <victor.90braz@gmail.com>
2023-11-14 18:00:31 +0100

    fix white space in the text area

--- resources/views/components/flat/create.blade.php
+++ resources/views/components/flat/create.blade.php
@@ -31,13 +31,13 @@
                 </select>
               </div>
             </div>
-          </div>
+        </div>
 
         <div>
-          <label for="description" class="block text-sm font-medium leading-6 text-gray-900">DESCRIPTION</label>
-          <div class="mt-2">
-            <textarea name="description" id="description" cols="50" rows="5" class="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
-          </div>
+            <label for="description" class="block text-sm font-medium leading-6 text-gray-900">DESCRIPTION</label>
+            <div class="mt-2">
+                <textarea name="description" id="description" cols="50" rows="5" class="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">{{ $flat->description }}</textarea>
+            </div>
         </div>
 
         <div>
