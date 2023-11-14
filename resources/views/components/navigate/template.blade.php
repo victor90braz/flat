0f3cd7a5cfe116649e95acfad5479d827f646f65
victor braz <victor.90braz@gmail.com>
2023-11-14 09:32:50 +0100

    add position fixed to feedback

--- resources/views/components/navigate/template.blade.php
+++ resources/views/components/navigate/template.blade.php
@@ -12,30 +12,30 @@
         <div class="hidden sm:block ml-6">
             <div class="flex space-x-4 items-center">
                 <div class="btm-nav">
-                    <button class="flex flex-col items-center">
+                    <a href="/" aria-current="page" class="flex flex-col items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                         </svg>
-                        <a href="/" aria-current="page">Home</a>
-                    </button>
+                        Home
+                    </a>
                 </div>
                 @if(Auth::user())
                     <div class="btm-nav">
-                        <button class="flex flex-col items-center">
+                        <a href="/flats" aria-current="page" class="flex flex-col items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 21h18v-6H3zm0 0H21V9h-3.5M12 3L2 9v12h4v-6h8v6h4V9L12 3z"/>
                             </svg>
-                            <a href="/flats" aria-current="page">My Flats</a>
-                        </button>
+                            My Flats
+                        </a>
                     </div>
 
                     <div class="btm-nav">
-                        <button class="flex flex-col items-center">
+                        <a href="/logout" class="flex flex-col items-center text-red-600 hover:text-red-700" aria-current="page">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"/>
                             </svg>
-                            <a href="/logout" class="flex items-center text-red-600 hover:text-red-700" aria-current="page">Logout</a>
-                        </button>
+                            Logout
+                        </a>
                     </div>
                 @endif
             </div>
