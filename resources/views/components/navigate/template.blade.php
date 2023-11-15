16d40aa88b3971eeb580781c68f05a36c818c4c6
victor braz <victor.90braz@gmail.com>
2023-11-15 12:50:54 +0100

    add black color to menu icon

--- resources/views/components/navigate/template.blade.php
+++ resources/views/components/navigate/template.blade.php
@@ -53,7 +53,7 @@
 
         <div class="sm:hidden">
             <button id="mobile-menu-button" class="text-gray-500 hover:text-white focus:outline-none focus:text-white">
-                <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <svg class="h-6 w-6 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
                 </svg>
             </button>
