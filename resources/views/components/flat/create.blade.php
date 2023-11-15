d9458e7f897207e6095661a7162950ec3972d884
victor braz <victor.90braz@gmail.com>
2023-11-15 12:48:02 +0100

    remove coins

--- resources/views/components/flat/create.blade.php
+++ resources/views/components/flat/create.blade.php
@@ -25,8 +25,6 @@
               <div class="absolute inset-y-0 right-0 flex items-center">
                 <label for="currency" class="sr-only">Currency</label>
                 <select id="currency" name="currency" class="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
-                  <option>USD</option>
-                  <option>CAD</option>
                   <option>EUR</option>
                 </select>
               </div>
