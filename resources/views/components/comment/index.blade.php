dd12d2468693d349b3dd223a3541be83551e319a
victor braz <victor.90braz@gmail.com>
2023-11-15 18:41:32 +0100

    update image accordling with user id

--- resources/views/components/comment/index.blade.php
+++ resources/views/components/comment/index.blade.php
@@ -3,7 +3,7 @@
         <div class="mt-4 border p-4 bg-blue-200">
             <article class="flex">
                 <div class="flex flex-col mr-4" style="display: flex; flex-direction: column; align-items: center;">
-                    <img src="https://i.pravatar.cc/100?img=4" alt="avatar" class="rounded-full">
+                    <img src="https://i.pravatar.cc/100?img={{$comment->user->id}}" alt="avatar" class="rounded-full">
 
                     <header class="flex flex-col items-center">
                         <h3 class="font-bold">{{ $comment->user->name }}</h3>
