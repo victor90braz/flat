57ccef9e61b910f9c663e93c70bb95ce0b29a367
victor braz <victor.90braz@gmail.com>
2023-11-15 17:44:50 +0100

    delete on cascade

--- database/migrations/2023_11_09_144628_create_flats_table.php
+++ database/migrations/2023_11_09_144628_create_flats_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('location');
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
--- database/migrations/2023_11_15_161507_create_comments_table.php
+++ database/migrations/2023_11_15_161507_create_comments_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->unsignedBigInteger('flat_id');
             $table->text('body');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('flat_id')->references('id')->on('flats')->onDelete('cascade');
         });
     }
 
