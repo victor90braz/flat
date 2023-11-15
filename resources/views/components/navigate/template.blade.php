23e82083f346a8699a973fe832de4311b32efba4
victor braz <victor.90braz@gmail.com>
2023-11-15 12:47:02 +0100

    remove undefined description

--- resources/views/components/navigate/template.blade.php
+++ resources/views/components/navigate/template.blade.php
@@ -30,7 +30,8 @@
                     </div>
 
                     <div>
-                        <a href="/flat/create" aria-current="page" class="flex flex-col items-center">
+
+                        <a href="{{ url('/flat/create') }}" aria-current="page" class="flex flex-col items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
                             </svg>
