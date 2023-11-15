e24448edec806aa9601e5b84d473479d1af3896c
victor braz <victor.90braz@gmail.com>
2023-11-15 17:17:53 +0100

    create database table

--- database/migrations/2023_11_15_161507_create_comments_table.php
+++ database/migrations/2023_11_15_161507_create_comments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('flat_id');
+            $table->text('body');
             $table->timestamps();
         });
     }
