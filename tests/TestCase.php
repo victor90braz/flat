6671863f91c34791666cb9e6c3b8c32246fa6e24
victor braz <victor.90braz@gmail.com>
2023-11-17 11:30:28 +0100

    create a unit test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,22 @@
 
 namespace Tests;
 
+use App\Calculator;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function it_can_add_two_numbers()
+    {
+        // Arrange
+        $calculator = new Calculator();
+
+        // Act
+        $result = $calculator->add(5, 10);
+
+        // Assert
+        $this->assertEquals(15, $result);
+    }
 }
