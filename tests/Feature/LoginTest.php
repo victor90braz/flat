37e57a22cf83aaa8de3f09da0e57cc63e5f2cd3a
victor braz <victor.90braz@gmail.com>
2023-11-17 16:34:46 +0100

    cover error message

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -13,38 +13,31 @@ class LoginTest extends TestCase
     /** @test */
     public function a_user_can_login_with_valid_credentials()
     {
-        // Create a user
         $user = User::factory()->create([
             'email' => 'test@example.com',
             'password' => bcrypt('password123'),
         ]);
 
-        // Attempt to login with valid credentials
         $response = $this->post('/login', [
             'email' => 'test@example.com',
             'password' => 'password123',
         ]);
 
-        // Assert that the user is redirected to the home page
         $response->assertRedirect('/');
 
-        // Assert that the user is authenticated
         $this->assertAuthenticatedAs($user);
     }
 
     /** @test */
     public function a_user_cannot_login_with_invalid_credentials()
     {
-        // Attempt to login with invalid credentials
         $response = $this->post('/login', [
             'email' => 'nonexistent@example.com',
             'password' => 'invalidpassword',
         ]);
 
-        // Assert that the response has validation errors
         $response->assertSessionHasErrors('email');
 
-        // Assert that the user is not authenticated
         $this->assertGuest();
     }
 }
