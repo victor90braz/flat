934b6584239705b9b4a48a8205369b8fab82f1b4
victor braz <victor.90braz@gmail.com>
2023-11-20 15:46:28 +0100

    tested FlatControllerTest

--- tests/Feature/Controllers/FlatControllerTest.php
+++ tests/Feature/Controllers/FlatControllerTest.php
@@ -14,19 +14,13 @@ class FlatControllerTest extends TestCase
     /** @test */
     public function it_displays_a_list_of_flats_on_index()
     {
-        // Create some flats in the database
         Flat::factory(5)->create();
 
-        // Make a GET request to the index method
         $response = $this->get('/');
 
-        // Assert that the response has a successful status code
         $response->assertStatus(200);
 
-        // Assert that the flats are displayed on the page
         $response->assertViewIs('pages.flats.index');
         $response->assertViewHas('flats');
     }
-
-
 }
