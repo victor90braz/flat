02cf50085e4ad7491b20145b2baf7dd3350667ad
victor braz <victor.90braz@gmail.com>
2023-11-20 17:09:51 +0100

    tested FlatCommentTest

--- tests/Feature/Views/Components/Comments/FlatCommentTest.php
+++ tests/Feature/Views/Components/Comments/FlatCommentTest.php
@@ -2,17 +2,37 @@
 
 namespace Tests\Feature\Views\Components\Comments;
 
+use App\Models\Comment;
+use App\Models\Flat;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class FlatCommentTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function authenticated_user_can_post_comment()
+    {
+        // Arrange
+        $flat = Flat::factory()->create();
+        $user = User::factory()->create();
+
+        // Act
+        $response = $this->actingAs($user)
+            ->post(route('flats.comments.store', ['flat' => $flat->id]), [
+                'body' => 'This is a test comment.',
+            ]);
+
+        // Assert
+        $response->assertStatus(302);
+
+        $this->assertDatabaseHas('comments', [
+            'flat_id' => $flat->id,
+            'user_id' => $user->id,
+            'body' => 'This is a test comment.',
+        ]);
     }
+
 }
