2c4922076b6411a39980c4feafbc13b9d0f6fe1a
victor braz <victor.90braz@gmail.com>
2023-11-20 16:50:59 +0100

    namespace Tests\Controllers\Feature;

--- tests/Feature/Controllers/FlatCommentTest.php
+++ tests/Feature/Controllers/FlatCommentTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Controllers;
+namespace Tests\Controllers\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
--- tests/Feature/Controllers/FlatTest.php
+++ tests/Feature/Controllers/FlatTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Controllers;
+namespace Tests\Controllers\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
