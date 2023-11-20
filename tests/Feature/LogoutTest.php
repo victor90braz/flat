7c4b7a585edd3667a16fe89bc6a9473e9bc16b69
victor braz <victor.90braz@gmail.com>
2023-11-20 15:28:26 +0100

    tested logout

---
+++ tests/Feature/LogoutTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+
+class LogoutTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_logout()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $response = $this->get('/logout');
+
+        $response->assertRedirect('/');
+    }
+}
