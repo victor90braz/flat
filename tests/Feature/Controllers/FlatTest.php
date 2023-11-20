f8201f63837aae4885f729b9c01f5ef0a5208bde
victor braz <victor.90braz@gmail.com>
2023-11-20 17:08:10 +0100

    namespace Tests\Feature\Controllers;

--- tests/Feature/Controllers/FlatTest.php
+++ tests/Feature/Controllers/FlatTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Controllers\Feature;
+namespace Tests\Feature\Controllers;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
