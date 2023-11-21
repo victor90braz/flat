aaf1ce76aa045d5ca79b9ad0d7653e8bb6c099e4
victor braz <victor.90braz@gmail.com>
2023-11-21 12:31:28 +0100

    it_fails_validation_with_missing_required_data

---
+++ tests/Feature/Requests/StoreFlatRequestTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature\Requests\StoreFlatRequest;
+
+use App\Http\Requests\StoreFlatRequest;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class StoreFlatRequestTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_passes_validation_with_valid_data()
+    {
+        $data = [
+            'title' => 'Sample Flat',
+            'price' => 1000,
+            'description' => 'This is a sample flat description.',
+            'location' => 'Sample Location',
+        ];
+
+        $response = $this->post(route('flats.store'), $data);
+
+        $response->assertSessionHasNoErrors();
+        $response->assertStatus(302); // Assuming a redirect after successful validation
+    }
+
+    /** @test */
+    public function it_fails_validation_with_missing_required_data()
+    {
+        $data = [
+            // Missing 'title', 'price', 'description', 'location'
+        ];
+
+        $response = $this->post(route('flats.store'), $data);
+
+        $response->assertSessionHasErrors(['title', 'price', 'description', 'location']);
+        $response->assertStatus(302); // Assuming a redirect after failed validation
+    }
+
+    /** @test */
+    public function it_fails_validation_with_invalid_data()
+    {
+        $data = [
+            'title' => 'Sample Flat',
+            'price' => 'not_a_number', // Invalid data type for 'price'
+            'description' => 'This is a sample flat description.',
+            'location' => 'Sample Location',
+        ];
+
+        $response = $this->post(route('flats.store'), $data);
+
+        $response->assertSessionHasErrors('price');
+        $response->assertStatus(302); // Assuming a redirect after failed validation
+    }
+}
