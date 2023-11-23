5535ea7f5576265a051e6f499b20601128748700
victor braz <victor.90braz@gmail.com>
2023-11-23 15:56:15 +0100

    create a searchFlat component

--- resources/views/pages/flats/index.blade.php
+++ resources/views/pages/flats/index.blade.php
@@ -34,15 +34,7 @@
 
     <main>
 
-        <div class="relative flex h-16 items-center justify-between ml-4 mr-4">
-            <form action="#" method="GET">
-                @csrf
-                <input type="text" name="search" class="rounded-md px-3 py-1.5" placeholder="Find something" value="{{request('search')}}">
-                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                    Search
-                </button>
-            </form>
-        </div>
+        @include('components.search.searchFlat')
 
         @if ($flats->count() > 0)
             <ul class="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4 mt-6">
