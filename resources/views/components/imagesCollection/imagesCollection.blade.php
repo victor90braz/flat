eb26141fd61c64c59b0b94394802faab3c75cd67
victor braz <victor.90braz@gmail.com>
2023-11-24 10:39:37 +0100

    create an imagesCollection component

---
+++ resources/views/components/imagesCollection/imagesCollection.blade.php
@@ -0,0 +1,26 @@
+<div class="gap-2 mx-auto p-2 mt-4 sm:px-6 lg:grid lg:max-w-8xl lg:grid-cols-3">
+
+    <div class="sm:w-full lg:w-auto lg:col-span-1 mb-4 lg:mb-0 lg:gap-4">
+        <div class="aspect-h-4 aspect-w-5 overflow-hidden rounded-lg lg:block">
+            <img src="https://plus.unsplash.com/premium_photo-1680100256112-2e1231d9d0df?q=80&amp;w=1887&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="w-full h-full object-cover object-center">
+        </div>
+    </div>
+
+    <div class=" gap-2 hidden sm:grid sm:grid-cols-2 lg:grid lg:px-0">
+
+        <div class="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
+            <img src="https://plus.unsplash.com/premium_photo-1680100256112-2e1231d9d0df?q=80&amp;w=1887&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Model wearing plain black basic tee." class="w-full h-full object-cover object-center">
+        </div>
+        <div class="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
+            <img src="https://plus.unsplash.com/premium_photo-1680100256112-2e1231d9d0df?q=80&amp;w=1887&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Model wearing plain gray basic tee." class="w-full h-full object-cover object-center">
+        </div>
+
+    </div>
+
+    <div class="sm:w-full lg:w-auto lg:col-span-1 mt-4 lg:mt-0">
+        <div class="aspect-h-4 aspect-w-5 overflow-hidden rounded-lg lg:block">
+            <img src="https://plus.unsplash.com/premium_photo-1680100256112-2e1231d9d0df?q=80&amp;w=1887&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="w-full h-full object-cover object-center">
+        </div>
+    </div>
+
+</div>
