2a1142b39ea67674c006e643066258711dbcb42a
victor braz <victor.90braz@gmail.com>
2023-11-24 10:23:14 +0100

    fix css

--- resources/views/pages/flats/detail.blade.php
+++ resources/views/pages/flats/detail.blade.php
@@ -31,30 +31,21 @@
     @endauth
 
     <div class="mx-auto p-4 mt-6 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-        <div class="aspect-h-4 aspect-w-5 hidden overflow-hidden rounded-lg lg:block">
-            <img src="{{$images[0]}}" class="w-full h-full object-cover object-center">
+        <div class="aspect-h-4 aspect-w-5 overflow-hidden rounded-lg lg:block">
+            <img src="https://plus.unsplash.com/premium_photo-1680100256112-2e1231d9d0df?q=80&amp;w=1887&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="w-full h-full object-cover object-center">
         </div>
 
-        <div class="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
+        <div class=" lg:grid lg:grid-cols-1 lg:gap-y-8">
             <div class="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-                <img src="{{$images[0]}}" alt="Model wearing plain black basic tee." class="w-full h-full object-cover object-center">
+                <img src="https://plus.unsplash.com/premium_photo-1680100256112-2e1231d9d0df?q=80&amp;w=1887&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Model wearing plain black basic tee." class="w-full h-full object-cover object-center">
             </div>
             <div class="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-                <img src="{{$images[0]}}" alt="Model wearing plain gray basic tee." class="w-full h-full object-cover object-center">
-            </div>
-        </div>
-
-        <div class="lg:hidden mt-4">
-            <div class="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-                <img src="{{$images[0]}}" alt="Model wearing plain black basic tee." class="w-full h-full object-cover object-center">
-            </div>
-            <div class="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg mt-4">
-                <img src="{{$images[0]}}" alt="Model wearing plain gray basic tee." class="w-full h-full object-cover object-center">
+                <img src="https://plus.unsplash.com/premium_photo-1680100256112-2e1231d9d0df?q=80&amp;w=1887&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Model wearing plain gray basic tee." class="w-full h-full object-cover object-center">
             </div>
         </div>
 
-        <div class="aspect-h-4 aspect-w-5 hidden overflow-hidden rounded-lg lg:block">
-            <img src="{{$images[0]}}" class="w-full h-full object-cover object-center">
+        <div class="aspect-h-4 aspect-w-5 overflow-hidden rounded-lg lg:block">
+            <img src="https://plus.unsplash.com/premium_photo-1680100256112-2e1231d9d0df?q=80&amp;w=1887&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="w-full h-full object-cover object-center">
         </div>
     </div>
 
