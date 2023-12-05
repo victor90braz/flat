dbd9526829197868d76aaa92002d8647207a7285
victor braz <victor.90braz@gmail.com>
2023-12-05 18:27:50 +0100

    add image to users

--- resources/views/components/users/users.blade.php
+++ resources/views/components/users/users.blade.php
@@ -5,7 +5,7 @@
         @foreach ($users->reverse() as $user)
         <li class="flex justify-between gap-x-6 py-5 m-6">
             <div class="flex min-w-0 gap-x-4">
-                <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="{{$image[0]}}" alt="User profile">
                 <div class="min-w-0 flex-auto">
                 <p class="text-sm font-semibold leading-6 text-gray-900">{{$user->name}}</p>
                 <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{$user->email}}</p>
